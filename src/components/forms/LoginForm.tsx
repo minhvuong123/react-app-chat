4c03da4b580f0ecbe11b2264f63ae07b50cff2c2
minhvuong123 <minhvuong12222@gmail.com>
2024-01-10 23:26:16 +0700

    add react-hook-form for Login and Register page

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,21 +1,23 @@
 import { Link } from "react-router-dom";
 import { Button, InputContainer, InputField, InputLabel } from "../../utils/styles";
 import styles from "./index.module.scss";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 export const LoginForm = () => {
-<></>
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    
   }
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <InputContainer>
         <InputLabel htmlFor="email">Email</InputLabel>
-        <InputField id="email" type="text" />
+        <InputField id="email" type="text" {...register('email', { required: "Email is required" })} />
       </InputContainer>
       <InputContainer className={styles.loginFormPassword}>
         <InputLabel htmlFor="password">Password</InputLabel>
-        <InputField id="password" type="text" />
+        <InputField id="password" type="text" {...register('password', { required: "Password is required" })} />
       </InputContainer>
       <Button>Login</Button>
       <div className={styles.footerText}>
--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,31 +1,34 @@
 import { Link } from "react-router-dom";
 import { Button, InputContainer, InputField, InputLabel } from "../../utils/styles";
 import styles from "./index.module.scss";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 export const RegisterForm = () => {
-<></>
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    
   }
+
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <InputContainer>
         <InputLabel htmlFor="email">Email</InputLabel>
-        <InputField id="email" type="text" />
+        <InputField id="email" type="text" {...register('email', { required: "Email is required" })} />
       </InputContainer>
       <section className={styles.nameFieldRow}>
         <InputContainer>
           <InputLabel htmlFor="firstName">First Name</InputLabel>
-          <InputField id="firstName" type="text" />
+          <InputField id="firstName" type="text" {...register('firstName', { required: "First Name is required" })} />
         </InputContainer>
         <InputContainer>
           <InputLabel htmlFor="lastName">Last Name</InputLabel>
-          <InputField id="lastName" type="text" />
+          <InputField id="lastName" type="text" {...register('lastName', { required: "Last Name is required" })} />
         </InputContainer>
       </section>
       <InputContainer>
         <InputLabel htmlFor="password">Password</InputLabel>
-        <InputField id="password" type="text" />
+        <InputField id="password" type="text" {...register('password', { required: "Password is required" })} />
       </InputContainer>
       <Button className={styles.button}>Create My Account</Button>
       <div className={styles.footerText}>
